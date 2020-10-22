f7ab688c76469b4d3c6340d47617bff24d404818
root <root@YuaHa-PD.localdomain>
2020-10-22 21:45:40 +0800

    charpt 3

--- demo01.py
+++ demo01.py
@@ -1,2 +1,22 @@
-a = "hello world"
-print (a)
+def collatz(number):
+    print (number)
+    if number%2 == 0:
+        return int(number/2)
+    else:
+        return number*3+1
+
+
+errNum = 1
+while errNum == 1:
+    try:
+        i = int(input('Input your number:'))
+        errNum = 0
+    except ValueError:
+        errNum = 1
+
+while i != 1:
+    i = collatz(i)
+print (1)
+
+
+        
