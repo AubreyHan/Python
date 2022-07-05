c822dd571cd1a34c461c64c68f7817b1dd57ad1d
Aubrey Han <aubreyhan@live.com>
2022-07-05 07:22:28 +0000

    demo

---
+++ MyCode/demo01.py
@@ -0,0 +1,22 @@
+
+from google.cloud import storage
+
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
+
+    print("Blobs:")
+    for blob in blobs:
+        print(blob.name)
+
+    if delimiter:
+        print("Prefixes:")
+        for prefix in blobs.prefixes:
+            print(prefix)
+
+list_blobs_with_prefix('hydemo002','demo')
+
